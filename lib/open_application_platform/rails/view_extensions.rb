e3990b6ccc7db5140b0ebee55aaa4f3a0132af7e
Sam Pohlenz <sam@gooddogdesign.com>
2008-04-21 16:21:13 -0700

    View fix

--- lib/open_application_platform/rails/view_extensions.rb
+++ lib/open_application_platform/rails/view_extensions.rb
@@ -5,7 +5,7 @@ module OpenApplicationPlatform::Rails::ViewExtensions
   
   def url_for_with_canvas_support(*args)
     returning url_for_without_canvas_support(*args) do |url|
-      url.gsub!(/^\//, canvas_path) if in_canvas? && url !~ /^#{controller.canvas_path}/
+      url.gsub!(/^\//, canvas_path) if in_canvas? && url !~ /^#{canvas_path}/
     end
   end
   
